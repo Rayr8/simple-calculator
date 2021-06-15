a687bcc4f0b5ceaee5683c0cd0d7e644139d1692
raeesah <raeesahr8@gmail.com>
2021-06-15 14:32:24 +0200

    Added functions to multiply and divide numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+//Addition
 function add(x, y) {
     let sum = x + y;
     console.log(sum);
@@ -5,6 +6,7 @@ function add(x, y) {
 
 add(5, 3);
 
+//Subtraction
 function subtract(x, y) {
     let subtract = x - y;
     console.log(subtract);
@@ -12,4 +14,19 @@ function subtract(x, y) {
 
 subtract(10, 3);
 
+//Multiplication
+function multiply(x, y) {
+    let multiply = x * y;
+    comsole.log(multiply);
+}
 
+multiply(5, 2);
+
+//Division 
+function divide(x, y) {
+    let divide = x / y;
+    console.log(divide);
+}
+ divide(20, 10);
+
+ 
